44d8eea06b6dc86864c5e5a1dd0c01355d7ba907
Roberto Silva E. <jamesclaus858@gmail.com>
2026-06-12 15:47:22 -0400

    Add music player functionality to Avance.js

--- Avance.js
+++ Avance.js
@@ -1,2 +1,19 @@
 // Avance 12/6/2026
 
+//testing de reproductor de musica
+
+let mySound;
+
+function preload() {
+  soundFormats('mp3');
+  rooster = loadSound('NetDriver.mp3');
+}
+
+function setup() {
+  createCanvas(300, 300);
+  background(220);
+}
+
+function mousePressed() {
+  rooster.play();
+}
